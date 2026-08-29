3639f6aa268ab3afcdc4ccf695dc997135f0ae28
sheranii <194492330+sheranii@users.noreply.github.com>
2026-08-29 23:59:41 +0530

    Time: 154 ms (44.48%), Space: 77.2 MB (11.54%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
@@ -1,34 +1,24 @@
 class Solution {
     public List<List<Integer>> findPrimePairs(int n) {
-        boolean[] prime = new boolean[n+1];
-        java.util.Arrays.fill(prime, true);
-        prime[0] = prime[1] = false;
-        for (int i = 2; i * i <= n; i++) {
-            if (prime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    prime[j] = false;
+        int[] prime= new int[n+1];
+        List<List<Integer>> ans = new ArrayList<>();
+        if (n < 4) return ans;
+
+        Arrays.fill(prime,1);
+        prime[0]=0;
+        prime[1]=1;
+        for(int i=2; i*i<=n; i++){
+            if(prime[i]==1){
+                for(int j=i*i; j<=n; j+=i){
+                    prime[j]=0;
                 }
             }
         }
-        
-        List<Integer> primes = new java.util.ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            if (prime[i]) primes.add(i);
-        }
-        
-        List<List<Integer>> ans = new java.util.ArrayList<>();
-        int left = 0, right = primes.size()-1;
-        while (right >= left) {
-            int sum = primes.get(left) + primes.get(right);
-            if (sum == n) {
-                ans.add(java.util.Arrays.asList(primes.get(left), primes.get(right)));
-                left++;
-            } else if (sum > n) {
-                right--;
-            } else {
-                left++;
+         for (int i = 2; i <= n / 2; i++) {
+            if (prime[i]==1 && prime[n - i]==1) {
+                ans.add(Arrays.asList(i, n - i));
             }
-        }
+         }
         return ans;
     }
 }
