08f9ba3c13d85c1cf2c2a9763842f2aab1e125d1
sheranii <194492330+sheranii@users.noreply.github.com>
2026-09-27 01:01:58 +0530

    Time: 19 ms (6.47%), Space: 68.4 MB (9.83%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,19 +1,39 @@
 import java.util.*;
-
 class Solution {
-    public List<Integer> grayCode(int n) {
-
-        List<Integer> result = new ArrayList<>();
-        result.add(0);
+    public boolean solve(int n, List<Integer> ans, Set<Integer> used) {
+        if (ans.size() == (1 << n)) {
+            return Integer.bitCount(ans.get(0) ^ ans.get(ans.size() - 1)) == 1;
+        }
+        int current = ans.get(ans.size() - 1);
+        //flipping
         for (int bit = 0; bit < n; bit++) {
+            int next = current ^ (1 << bit);
 
-            int mask = 1 << bit;
+            if (!used.contains(next)) {
 
-            for (int i = result.size() - 1; i >= 0; i--) {
-                result.add(result.get(i) | mask);
+                used.add(next);
+                ans.add(next);
+
+                if (solve(n, ans, used))
+                    return true;
+                ans.remove(ans.size() - 1);
+                used.remove(next);
             }
         }
 
-        return result;
+        return false;
+    }
+
+    public List<Integer> grayCode(int n) {
+
+        List<Integer> ans = new ArrayList<>();
+        Set<Integer> used = new HashSet<>();
+
+        ans.add(0);
+        used.add(0);
+
+        solve(n, ans, used);
+
+        return ans;
     }
 }
